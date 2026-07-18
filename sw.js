65a55abba2942e842709229c76c5837f6ba58baf
rakibxh <hassanxrakib@gmail.com>
2026-07-18 20:28:00 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,8 +13,8 @@ const APP_SHELL = [
   "./style.css",
   "./app.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 // Cache the app shell on install
